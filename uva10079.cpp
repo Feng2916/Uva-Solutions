d05fe93829058ffb3c317847042de10380b039a5
Feng <ktfeng29160@gmail.com>
2026-03-16 10:19:12 +0800

    solved 10079

---
+++ uva10079.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    long long N;
+    while (cin >> N && N >= 0) cout << 1+N*(N+1)/2 << '\n';
+}
