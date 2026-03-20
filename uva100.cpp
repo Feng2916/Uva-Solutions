4646aa96cda2f02485f4d2fdd9d7fa9582ee8392
Feng <ktfeng29160@gmail.com>
2026-03-21 00:41:13 +0800

    solved 100

--- uva100.cpp
+++ uva100.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector <int> val (1e6, 0);
+vector <bool> rd (1e6, 0);
+
 int alg (int n) {
     int cnt = 1;
-    while (n != 1) {
-        if (n % 2) {
-            n = (3*n + 1)/2;
+    int N = n;
+    while (n!=1) {
+        if (n<1e6 && rd[n]) {
+            cnt += val[n]-1;
+            break;
+        }
+        else if (n%2) {
+            n = (3*n+1)/2;
             cnt += 2;
         } else {
             n /= 2;
             ++cnt;
         }
+    }
+    if (N<1e6) {
+        val[N] = cnt;
+        rd[N] = 1;
     }
     return cnt;
 }
@@ -21,11 +33,9 @@ int main () {
     int i, j;
     while (cin >> i >> j) {
         int maxlen = INT_MIN;
-        for (int k = min(i, j); k <= max(i,j); ++k) {
+        for (int k = min (i, j); k <= max (i, j); ++k) {
             int len = alg(k);
-            if (len > maxlen) {
-                maxlen = len;
-            }
+            if (len > maxlen) maxlen = len;
         }
         cout << i << ' ' << j << ' ' << maxlen << '\n';
     }
